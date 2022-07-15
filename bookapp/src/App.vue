95a3c5a6c51bd072fcb9c3082fd4de6e16b9920e
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-07-15 10:48:24 +0900

    add save method of book info received from google book api to local strage

--- bookapp/src/App.vue
+++ bookapp/src/App.vue
@@ -1,25 +1,61 @@
 <template>
   <v-app>
-    <Header/>
+    <Header />
     <v-main>
-      <router-view/>
+      <v-container class="mt-5">
+        <router-view @add-book-list="addBook" />
+      </v-container>
     </v-main>
-    <Footer/>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import Header from '@/global/Header';
 import Footer from '@/global/Footer';
-
+const STORAGE_KEY = 'books';
 export default {
   name: 'App',
   components: {
     Header,
     Footer
   },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      books: [],
+      newBook: null
+    }
+  },
+  mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch (e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: '',
+        memo: ''
+      });
+      // this.newBook = "";
+      this.saveBooks();
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    }
+  }
 };
 </script>
--- bookapp/src/pages/BookSearch.vue
+++ bookapp/src/pages/BookSearch.vue
@@ -1,93 +1,100 @@
 <template>
   <div>
-    <v-container class="mt-5">
-      <v-row>
-        <v-col cols="3"></v-col>
-        <v-col cols="6">
-          <v-text-field outlined label="タイトル検索" v-model = "keyword"></v-text-field>
-        </v-col>
-        <v-col cols="3"></v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="3"></v-col>
-        <v-col cols="3">
-          <div align="center">
-            <v-btn color="primary" @click="search(keyword)">
-              検索
-            </v-btn>
-          </div>
-        </v-col>
-        <v-col cols="3">
-          <div align="center">
-            <v-btn color="secondary" to="/">
-              一覧に戻る
-            </v-btn>
-          </div>
-        </v-col>
-        <v-col cols="3"></v-col>
-      </v-row>
-      <div class="my-10"></div>
-      <v-row>
-        <v-col cols="12" md="6" v-for="(book, index) in searchResults" :key="book.index">
-          <v-card class="mx-auto">
-            <v-row>
-              <v-col cols="4">
-                <v-img :src="book.image"></v-img>
-              </v-col>
-              <v-col cols="8">
-                <v-card-title>{{ book.title }}</v-card-title>
-                {{ book.description }}
-                <v-spacer></v-spacer>
-                <v-card-actions>
-                  <v-btn fab dark color="indigo" @click="addBookList(index)">
-                    <v-icon dark>mdi-plus</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row>
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-text-field
+          outlined
+          label="タイトル検索"
+          v-model="keyword"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3"></v-col>
+      <v-col cols="3">
+        <div align="center">
+          <v-btn color="primary" @click="search(keyword)"> 検索 </v-btn>
+        </div>
+      </v-col>
+      <v-col cols="3">
+        <div align="center">
+          <v-btn color="secondary" to="/"> 一覧に戻る </v-btn>
+        </div>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+    <div class="my-10"></div>
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+        v-for="(book, index) in searchResults"
+        :key="book.index"
+      >
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>{{ book.title }}</v-card-title>
+              {{ book.description }}
+              <v-spacer></v-spacer>
+              <v-card-actions>
+                <v-btn fab dark color="indigo" @click="addBookList(index)">
+                  <v-icon dark>mdi-plus</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BookSearch',
+  name: "BookSearch",
   data() {
     return {
-      keyword: '',
-      searchResults: []
-    }
+      keyword: "",
+      searchResults: [],
+    };
   },
   methods: {
+    addBookList(index) {
+      this.$emit('add-book-list', this.searchResults[index]);
+    },
     async search(keyword) {
       this.searchResults = [];
-      const baseUrl = 'https://www.googleapis.com/books/v1/volumes?';
+      const baseUrl = "https://www.googleapis.com/books/v1/volumes?";
       const params = {
         q: `intitle:${keyword}`,
-        maxResults: 40
+        maxResults: 40,
       };
       const queryParams = new URLSearchParams(params);
       console.log(baseUrl + queryParams);
 
-      const response = await fetch(baseUrl + queryParams)
-        .then(response => response.json());
+      const response = await fetch(baseUrl + queryParams).then((response) =>
+        response.json()
+      );
       console.log(response.items);
       for (let book of response.items) {
         let title = book.volumeInfo.title;
         let img = book.volumeInfo.imageLinks;
         let description = book.volumeInfo.description;
         this.searchResults.push({
-          title: title ? title : '',
-          image: img ? img.thumbnail : '',
-          description: description ? description.slice(0, 40) : '',
+          title: title ? title : "",
+          image: img ? img.thumbnail : "",
+          description: description ? description.slice(0, 40) : "",
         });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
