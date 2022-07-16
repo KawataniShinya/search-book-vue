37ddfc992e6f536b3b402d8459c31c35c42058c7
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-07-16 09:46:00 +0900

    add personal memo erea to edit page

--- bookapp/src/pages/BookEdit.vue
+++ bookapp/src/pages/BookEdit.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    BookEdit
-    {{ book.title }}
-    {{ books }}
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mx-auto">
+          <v-row>
+            <v-col cols="4">
+              <v-img :src="book.image"></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-title>
+                Title : {{ book.title }}
+              </v-card-title>
+              Read Date :
+              Personal Memo :
+              <v-textarea solo class="mx-2" v-model="book.memo">
+                {{ book.memo }}
+              </v-textarea>
+              <v-card-actions>
+                <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+                <v-btn color="info">保存</v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
