e76914b643d9e5942362f85c71d419a54fa36e6b
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-07-16 09:30:56 +0900

    add beforeRouteEnter and nextTick

--- bookapp/src/pages/BookEdit.vue
+++ bookapp/src/pages/BookEdit.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     BookEdit
+    {{ book.title }}
     {{ books }}
   </div>
 </template>
@@ -10,6 +11,19 @@ export default {
   name: 'BookEdit',
   props: {
     books: Array
+  },
+  data() {
+    return {
+      book: ''
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      vm.$nextTick(() => {
+        vm.book = vm.books[vm.$route.params.id];
+        console.log(vm.book);
+      })
+    })
   }
 }
 </script>
